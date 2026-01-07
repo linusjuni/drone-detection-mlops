802339f0e218112d3f96074d4ab6b50fc2fb426c
Linus Juni <linus@juni.dk>
2026-01-07 12:11:02 +0100

    adds wandb stuff to settings

--- src/drone_detector_mlops/utils/settings.py
+++ src/drone_detector_mlops/utils/settings.py
@@ -13,6 +13,9 @@ class Settings(BaseSettings):
     IMAGENET_MEAN: list = Field([0.485, 0.456, 0.406], env="IMAGENET_MEAN")
     IMAGENET_STD: list = Field([0.229, 0.224, 0.225], env="IMAGENET_STD")
 
+    WANDB_PROJECT_NAME: str = Field("drone-detector-mlops", env="WANDB_PROJECT_NAME")
+    WANDB_API_KEY: str = Field(..., env="WANDB_API_KEY")
+
     class Config:
         env_file_encoding = "utf-8"
         case_sensitive = True
